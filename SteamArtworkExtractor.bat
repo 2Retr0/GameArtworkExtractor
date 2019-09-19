d37bad9b0241d6cb3db646695d974917233b3615
2Retr0 <commandoboss3030@gmail.com>
2019-09-19 15:06:37 -0700

    manual ID support

--- SteamArtworkExtractor.bat
+++ SteamArtworkExtractor.bat
@@ -3,12 +3,15 @@
 set gameidline=0
 set gameidlinedata=
 set gameid=0
-
 echo . . .
-echo Search a Steam game title! (Make sure it's spelled correctly)
+echo Search a Steam game title! (or type 'manual' for ID input)
 
 rem Input search and get the respective HTML Steam page
 set /p _inputname=^> 
+if /i "%_inputname%" equ "exit" exit
+if /i "%_inputname%" equ "manual" goto manualsearch else goto namedsearch
+
+:namedsearch
 set webinput=%_inputname: =+%
 wget --output-document %webinput%.txt https://store.steampowered.com/search/?term=%webinput%
 
@@ -26,13 +29,25 @@ echo ID is %gameid%!
 del %webinput%.txt
 goto type
 
+rem Use inputted ID as Steam game ID
+:manualsearch
+echo Enter Steam game ID or type 'exit' to restart...
+set /p _inputid=^> 
+if /i "%_inputid%" equ "exit" goto start
+set gameid=%_inputid%
+set _inputname=app %_inputid%
+set "var="&for /f "delims=0123456789" %%i in ("%_inputid%") do set var=%%i
+if defined var (echo %_inputid% is not numeric! & goto manualsearch) else (goto type)
+
+rem Select between game cover, background, or logo
 :type
 echo . . .
 echo Choose a type! (cover/bg/logo)
-set /p inputtype=^> 
-if /i "%inputtype%" equ "cover" goto cover
-if /i "%inputtype%" equ "bg" goto background
-if /i "%inputtype%" equ "logo" goto logo
+set /p _inputtype=^> 
+if /i "%_inputtype%" equ "exit" goto start
+if /i "%_inputtype%" equ "cover" goto cover
+if /i "%_inputtype%" equ "bg" goto background
+if /i "%_inputtype%" equ "logo" goto logo
 if not defined layoutstate echo what? & goto type
 
 :cover
